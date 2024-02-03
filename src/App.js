914586e76583122e431c4bcc173aa634b0581ce5
Rahul Shaw <myselfrahul6290@gmail.com>
2024-02-03 11:48:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+
+const App = () => {
+  const [status, setStatus] = useState(null);
+
+  const checkOnlineStatus = async () => {
+    try {
+      const response = await fetch('https://dummyjson.com/products/1');
+      // console.log(response.status)
+      if (response.status === 200) {
+        const currentTime = new Date().toLocaleTimeString();
+        setStatus(`Online - Last checked at ${currentTime}`);
+      }
+    } catch (error) {
+      setStatus('Offline');
+    }
+  };
+
+
+  useEffect(() => {
+ 
+    checkOnlineStatus();
+// call after 10sec
+   setInterval(() => {
+      checkOnlineStatus();
+    }, 10000);
+
+    
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Online Status:</h1>
+      <p>{status}</p>
     </div>
   );
-}
+};
 
 export default App;
+
+
+
+
